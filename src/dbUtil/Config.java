1eb665551ff3558344a2916ebcf2410db00fb760
RyunosukeSakurada <longzhizhu513@gmail.com>
2022-11-10 21:00:38 -0800

    first commit

---
+++ src/dbUtil/Config.java
@@ -0,0 +1,9 @@
+package dbUtil;
+
+public class Config {
+
+    protected static final String URL = "jdbc:postgresql://db.lndydsxkmjmzoshxngcs.supabase.co/postgres";
+    protected static final String USERNAME = "postgres";
+    protected static final String PASSWORD = "jdG3KiNqAFd2hqDy";
+    
+}
